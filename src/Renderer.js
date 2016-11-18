a0ead7dc81e2f42c7c6251a9b3119b58f4c82018
Brent Vatne <brentvatne@gmail.com>
2016-11-17 17:09:57 -0800

    Accelerometer kinda, but commented out

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,4 +1,4 @@
-import Exponent from 'exponent';
+import Exponent, { Accelerometer, Gyroscope } from 'exponent';
 import React from 'react';
 import { Alert, Dimensions, PanResponder } from 'react-native';
 
@@ -11,10 +11,22 @@ const height = (screenHeight / screenWidth) * width;
 
 import Assets from '../assets';
 
-export default class Scene extends React.Component {
+function lowPass(input, output) {
+  const ALPHA = 0.1;
+
+  if (!output) {
+    return { ...input };
+  }
+  output.x = output.x + ALPHA * (input.x - output.x);
+  return output;
+}
+
+export default class Renderer extends React.Component {
+  _rotateX = 0;
 
   componentWillMount() {
     this._initializeScene();
+    this._addGyroscopeListener();
     this._addLighting();
     this._addPaddle();
     this._addBall();
@@ -23,6 +35,26 @@ export default class Scene extends React.Component {
 
   shouldComponentUpdate() {
     return false;
+  }
+
+  _addGyroscopeListener() {
+    // let lastSample = new Date();
+
+    // Accelerometer.setUpdateInterval(16.6);
+    // Accelerometer.addListener(e => {
+    //   let dt = 0.16;
+
+    //   this._currentAccelerometerVal = lowPass(e, this._currentAccelerometerVal);
+
+    //   const alpha = dt / (.3 + dt);
+    //   const smoothedVx = (alpha * this._currentAccelerometerVal.x * 5000) + (1.0 - alpha);
+
+    //   this._rotateX = smoothedVx * dt;
+    // });
+  }
+
+  componentWillUnmount() {
+    Accelerometer.removeAllListeners();
   }
 
   _initializeScene = () => {
@@ -35,14 +67,8 @@ export default class Scene extends React.Component {
       10000,  // Far
     );
 
-    this._camera.position.z = 1000;
-    try {
-      console.log('rotation:');
-      console.log(this._camera.rotation);
-      // this._camera.rotation.x = 1.5;
-    } catch(e) {
-      alert('hi');
-    }
+    // this._camera.position.x = 1;
+    this._camera.position.z = 50;
     this._scene = new THREE.Scene();
   }
 
@@ -62,15 +88,21 @@ export default class Scene extends React.Component {
   }
 
   _addGrid = () => {
-    const geometry = new THREE.PlaneBufferGeometry(width, height, 15, 15);
+    const geometry = new THREE.BoxGeometry(width, height, width, 5, 5, 5);
     const mesh = new THREE.Object3D();
 
     mesh.add(new THREE.LineSegments(
       geometry,
       new THREE.LineBasicMaterial({
         color: 0xffffff,
-        transparent: true,
         opacity: 0.5
+      })
+    ));
+
+    mesh.add(new THREE.Mesh(
+      geometry,
+      new THREE.MeshBasicMaterial({
+        color: 0x000000
       })
     ));
 
@@ -79,7 +111,7 @@ export default class Scene extends React.Component {
   }
 
   _addPaddle = () => {
-    const geometry = new THREE.PlaneBufferGeometry(1, 0.2);
+    const geometry = new THREE.BoxGeometry(1, 0.2, 1);
     const mesh = new THREE.Object3D();
 
     mesh.add(new THREE.Mesh(
@@ -98,21 +130,34 @@ export default class Scene extends React.Component {
   }
 
   _addBall = () => {
-    const geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
-    const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      shading: THREE.FlatShading
+    })
     const sphere = new THREE.Mesh( geometry, material );
 
-    sphere.position.set(width / 2, height / 2 , 1);
+    sphere.position.set(width / 2, height / 2 , 5);
     this._scene.add(sphere);
   }
 
   _updatePaddlePosition() {
     this._paddle.position.set(
+      // width * 0.5,
       width * this.props.paddlePosition.__getValue(),
-      height - 0.5, 1,
+      height - 0.5,
+      5,
     );
 
-    // this._camera.rotation.x = 0.009;
+    // let rotation = (this.props.paddlePosition.__getValue() - 0.5) * 0.1;
+
+    // this._camera.position.x = rotation;
+    // this._camera.position.y = -rotation;
+    // this._scene.rotation.z = rotation;
+    // this._scene.rotation.x = -rotation;
+    this._scene.rotation.y = this._rotateX / 100.0;
   }
 
   _tick = (dt) => {
