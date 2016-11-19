55bdf516a33e678ffd6d3776f860ee26ac260bec
Brent Vatne <brentvatne@gmail.com>
2016-11-18 17:55:50 -0800

    lol no m4a

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ class App extends React.Component {
       let assets = [
         require('./assets/sounds/countdownBlip.mp3'),
         require('./assets/sounds/brickDeath.mp3'),
-        require('./assets/sounds/blip.m4a'),
+        require('./assets/sounds/blip.wav'),
       ];
 
       await Promise.all(
--- src/GameState.js
+++ src/GameState.js
@@ -199,7 +199,7 @@ class GameState {
 
         ball.ballVx = placementFactor * 5;
 
-        this._emitSound(require('../assets/sounds/blip.m4a'));
+        this._emitSound(require('../assets/sounds/blip.wav'));
         return ball;
       }
 
